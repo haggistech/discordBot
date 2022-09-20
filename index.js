afe66f869e049c1c25b2dac25c3db8cb4dc385f5
Mik McLean <31162640+haggistech@users.noreply.github.com>
2022-09-20 17:57:14 +0100

    Removed uneeded code

--- index.js
+++ index.js
@@ -9,32 +9,13 @@ const client = new Client({
 		GatewayIntentBits.GuildMembers,
 	],
 });
-const { request } = require('undici');
+
 const func = require("./components/functions.js") 
 
-
-
-let interval;
 client.on('message', async msg => {
   switch (msg.content) {
     case "ping":
       msg.reply("Pong!");
-      break;
-    case "!meme":
-      msg.channel.send("Here's your meme!");
-      const img = await getMeme();
-      msg.channel.send(img);
-      break;
-    case "!eye":
-      msg.channel.send("You are now subscribed to eye reminders.");
-       interval = setInterval (function () {
-        msg.channel.send("Please take an eye break now!")
-        .catch(console.error); 
-      }, 3600000); //every hour
-      break;
-    case "!stop":
-      msg.channel.send("I have stopped eye reminders.");
-      clearInterval(interval);
       break;
   }
 });
